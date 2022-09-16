eb54538e2d36c42b6ce6ff7dfb2f6ae864cee24a
ReginaKoinigg <01060819@edu.campus02.at>
2022-09-16 19:13:01 +0200

    initial commit

---
+++ src/HelloWorld.java
@@ -0,0 +1,29 @@
+public class HelloWorld {
+
+    // Main methode - main+TAB oder psvm+TAB
+
+    public static void main(String[] args) {
+
+        // sout+TAB
+        // Endung println - Zeilenumbruch automatisch // print ohne ln - keine Zeilenumbruch
+        // rote Punkt - breakpoint - Käfer rechts oben - Debugger
+
+        System.out.println("Hello World! ");
+        System.out.println( "Welcome to KONF 2022");
+        System.out.println("Zeile 3");
+        System.out.println("Zeile 4");
+        System.out.println("Zeile 4");
+        System.out.println("Zeile 4");
+        System.out.println("Zeile 4");
+        System.out.println("Zeile 4");
+
+        // File - Export - Projekt zu ZIP File - dient zur Abgabe in der Klausur
+
+        // STRG+D - auf der Zeile stehen die ich duplizieren will
+
+    }
+}
+// codepilot - erstellt Codes
+// Git hub -
+//
+
